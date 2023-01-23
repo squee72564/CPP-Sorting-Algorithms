512015f577b91b55bd3bc1734fd818272940281f
squee72564 <rodriguez.al183@gmail.com>
2023-01-23 11:45:30 -0800

    Reformatted main driver function and broke test cases down into separate functions based on type within the container classes.

--- test.cpp
+++ test.cpp
@@ -4,6 +4,14 @@
 
 #include "SortingAlgorithms.h"
 
+void testIntContainers();
+
+void testFloatContainers();
+
+void testCharContainers();
+
+void testStringContainers();
+
 template <typename T>
 std::ostream& operator<<(std::ostream&, const std::vector<T>&);
 
@@ -24,19 +32,22 @@ void shuffle(std::list<T>&);
 
 int main ()
 {
-	std::vector<int> intVec =    {6,5,4,3,2,1,0,7,8,9,10,11,12,13,14};
-	std::array<int, 15> intArr =    {6,5,4,3,2,1,0,7,8,9,10,11,12,13,14};
+	testIntContainers();
+	testFloatContainers();
+	testCharContainers();
+	testStringContainers();
 
-	std::list<float> floatList = {19.23f,18.523f,17.23f,15.44f,23.2f,41.00001f,41.00009f};
+	return 0;
+}
 
-	std::vector<char> charVec = {'f','s','r','u','o','a','b','d','c','x','z','u','y','t','j','k','m','n','e'};
-	std::list<char> charList = {'f','s','r','u','o','a','b','d','c','x','z','u','y','t','j','k','m','n','e'};
+void testIntContainers()
+{
+	std::vector<int> intVec = {6,5,4,3,2,1,0,7,8,9,10,11,12,13,14};
+	std::array<int, 15> intArr = {6,5,4,3,2,1,0,7,8,9,10,11,12,13,14};
+	std::list<int> intList = {6,5,4,3,2,1,0,7,8,9,10,11,12,13,14};
 
-	std::vector<std::string> stringVec = {"zaa", "zab", "zba", "zbc", "cef", "abc", "def"};
-	std::list<std::string> stringList = {"zaa", "zab", "zba", "zbc", "cef", "abc", "def"};
 
-
-	std::cout << "[ INT VEC ]\n";
+	std::cout << "[ INT VEC <]\n";
 	selectionSort(intVec);
 	std::cout << intVec << "\n";
 	shuffle(intVec);
@@ -54,7 +65,7 @@ int main ()
 	shuffle(intVec);
 
 
-	std::cout << "\n[ INT ARR ]\n";
+	std::cout << "\n[ INT ARR <]\n";
 	selectionSort(intArr);
 	std::cout << intArr << "\n";
 	shuffle(intArr);
@@ -71,26 +82,91 @@ int main ()
 	std::cout << intArr << "\n";
 	shuffle(intArr);
 
+	std::cout << "\n[ INT LIST >]\n";
+	selectionSort(intList, std::greater<int>());
+	std::cout << intList << "\n";
+	shuffle(intList);
 
-	std::cout << "\n[ FLOAT LIST ]\n";
-	selectionSort(floatList, std::greater<decltype(floatList)::value_type>());
+	bubbleSort(intList, std::greater<int>());
+	std::cout << intList << "\n";
+	shuffle(intList);
+
+	insertionSort(intList, std::greater<int>());
+	std::cout << intList << "\n";
+	shuffle(intList);
+
+	mergeSort(intList, std::greater<int>());
+	std::cout << intList << "\n";
+	shuffle(intList);
+}
+
+void testFloatContainers()
+{
+	std::vector<float> floatVec = {19.23f,18.523f,17.23f,15.44f,23.2f,41.00001f,41.00009f};
+	std::array<float, 7> floatArr = {19.23f,18.523f,17.23f,15.44f,23.2f,41.00001f,41.00009f};;
+	std::list<float> floatList = {19.23f,18.523f,17.23f,15.44f,23.2f,41.00001f,41.00009f};
+
+	std::cout << "\n[ FLOAT VEC <]\n";
+	selectionSort(floatVec);
+	std::cout << floatVec << "\n";
+	shuffle(floatVec);
+
+	bubbleSort(floatVec);
+	std::cout << floatVec << "\n";
+	shuffle(floatVec);
+
+	insertionSort(floatVec);
+	std::cout << floatVec << "\n";
+	shuffle(floatVec);
+
+	mergeSort(floatVec);
+	std::cout << floatVec << "\n";
+	shuffle(floatVec);
+
+
+	std::cout << "\n[ FLOAT ARR <]\n";
+	selectionSort(floatArr);
+	std::cout << floatArr << "\n";
+	shuffle(floatArr);
+
+	bubbleSort(floatArr);
+	std::cout << floatArr << "\n";
+	shuffle(floatArr);
+
+	insertionSort(floatArr);
+	std::cout << floatArr << "\n";
+	shuffle(floatArr);
+
+	mergeSort(floatArr);
+	std::cout << floatArr << "\n";
+	shuffle(floatArr);
+
+
+	std::cout << "\n[ FLOAT LIST >]\n";
+	selectionSort(floatList, std::greater<float>());
 	std::cout << floatList << "\n";
 	shuffle(floatList);
 
-	bubbleSort(floatList, std::greater<decltype(floatList)::value_type>());
+	bubbleSort(floatList, std::greater<float>());
 	std::cout << floatList << "\n";
 	shuffle(floatList);
 
-	insertionSort(floatList, std::greater<decltype(floatList)::value_type>());
+	insertionSort(floatList, std::greater<float>());
 	std::cout << floatList << "\n";
 	shuffle(floatList);
 
-	mergeSort(floatList, std::greater<decltype(floatList)::value_type>());
+	mergeSort(floatList, std::greater<float>());
 	std::cout << floatList << "\n";
 	shuffle(floatList);
+}
 
+void testCharContainers()
+{
+	std::vector<char> charVec = {'f','s','r','u','o','a','b','d','c','x','z','u','y','t','j','k','m','n','e'};
+	std::array<char, 19> charArr = {'f','s','r','u','o','a','b','d','c','x','z','u','y','t','j','k','m','n','e'};
+	std::list<char> charList = {'f','s','r','u','o','a','b','d','c','x','z','u','y','t','j','k','m','n','e'};
 
-	std::cout << "\n[ CHAR VEC ]\n";
+	std::cout << "\n[ CHAR VEC <]\n";
 	selectionSort(charVec);
 	std::cout << charVec << "\n";
 	shuffle(charVec);
@@ -107,26 +183,48 @@ int main ()
 	std::cout << charVec << "\n";
 	shuffle(charVec);
 
+	std::cout << "\n[ CHAR ARR <]\n";
+	selectionSort(charArr);
+	std::cout << charArr << "\n";
+	shuffle(charArr);
 
-	std::cout << "\n[ CHAR LIST ]\n";
-	selectionSort(charList, std::greater<decltype(charList)::value_type>());
+	bubbleSort(charArr);
+	std::cout << charArr << "\n";
+	shuffle(charArr);
+
+	insertionSort(charArr);
+	std::cout << charArr << "\n";
+	shuffle(charArr);
+
+	mergeSort(charArr);
+	std::cout << charArr << "\n";
+	shuffle(charArr);
+
+	std::cout << "\n[ CHAR LIST >]\n";
+	selectionSort(charList, std::greater<char>());
 	std::cout << charList << "\n";
 	shuffle(charList);
 
-	bubbleSort(charList, std::greater<decltype(charList)::value_type>());
+	bubbleSort(charList, std::greater<char>());
 	std::cout << charList << "\n";
 	shuffle(charList);
 
-	insertionSort(charList, std::greater<decltype(charList)::value_type>());
+	insertionSort(charList, std::greater<char>());
 	std::cout << charList << "\n";
 	shuffle(charList);
 
-	mergeSort(charList, std::greater<decltype(charList)::value_type>());
+	mergeSort(charList, std::greater<char>());
 	std::cout << charList << "\n";
 	shuffle(charList);
+}
 
+void testStringContainers()
+{
+	std::vector<std::string> stringVec = {"zaa", "zab", "zba", "zbc", "cef", "abc", "def"};
+	std::array<std::string, 7> stringArr = {"zaa", "zab", "zba", "zbc", "cef", "abc", "def"};
+	std::list<std::string> stringList = {"zaa", "zab", "zba", "zbc", "cef", "abc", "def"};
 
-	std::cout << "\n[ STR VEC ]\n";
+	std::cout << "\n[ STR VEC <]\n";
 	selectionSort(stringVec);
 	std::cout << stringVec << "\n";
 	shuffle(stringVec);
@@ -143,25 +241,41 @@ int main ()
 	std::cout << stringVec << "\n";
 	shuffle(stringVec);
 
+	
+	std::cout << "\n[ STR ARR <]\n";
+	selectionSort(stringArr);
+	std::cout << stringArr << "\n";
+	shuffle(stringArr);
 
-	std::cout << "\n[ STR LIST ]\n";
-	selectionSort(stringList, std::greater<decltype(stringList)::value_type>());
+	bubbleSort(stringArr);
+	std::cout << stringArr << "\n";
+	shuffle(stringArr);
+
+	insertionSort(stringArr);
+	std::cout << stringArr << "\n";
+	shuffle(stringArr);
+
+	mergeSort(stringArr);
+	std::cout << stringArr << "\n";
+	shuffle(stringArr);
+
+
+	std::cout << "\n[ STR LIST >]\n";
+	selectionSort(stringList, std::greater<std::string>());
 	std::cout << stringList << "\n";
 	shuffle(stringList);
 
-	bubbleSort(stringList, std::greater<decltype(stringList)::value_type>());
+	bubbleSort(stringList, std::greater<std::string>());
 	std::cout << stringList << "\n";
 	shuffle(stringList);
 
-	insertionSort(stringList, std::greater<decltype(stringList)::value_type>());
+	insertionSort(stringList, std::greater<std::string>());
 	std::cout << stringList << "\n";
 	shuffle(stringList);
 
-	mergeSort(stringList, std::greater<decltype(stringList)::value_type>());
+	mergeSort(stringList, std::greater<std::string>());
 	std::cout << stringList << "\n";
 	shuffle(stringList);
-
-	return 0;
 }
 
 template <typename T>
@@ -216,4 +330,3 @@ void shuffle(std::list<T>& list)
 
 	std::move(temp.begin(), temp.end(), std::back_inserter(list)); 
 }
-
